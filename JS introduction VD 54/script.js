51243b0853845699caae5d66f1886a5917da2c62
yashxyz2611 <yashsinghthakur2611@gmail.com>
2025-04-05 20:16:15 +0530

    OPEN

---
+++ JS introduction VD 54/script.js
@@ -0,0 +1,25 @@
+alert(
+
+    "wellcome"
+);
+console.log(
+    "hey i am console" 
+)
+console.log(
+    "code is runing" 
+)
+console.log(
+    "code is runing..." 
+)
+var a = prompt("enter your number" )
+var Istrue = confirm("are you sure you want to leave this page and blast your computer")
+if(Istrue){
+    console.log("computer is blasting")
+
+}
+else{
+    console.log(
+        "computer is not blasting"
+    )
+}
+console.log("your number is" +a)
