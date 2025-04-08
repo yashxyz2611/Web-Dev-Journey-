6b0c59d8fcffb8988494fb2c279ed168ac2ab78c
yashxyz2611 <yashsinghthakur2611@gmail.com>
2025-04-08 14:14:29 +0530

    OPEN

---
+++ Conditionals VD 56/index.js
@@ -0,0 +1,34 @@
+console.log("hello")
+
+let age = 1;
+// let grace = 2;
+// age += grace
+// console.log(grace)
+// console.log(age + grace)
+// console.log(age - grace)
+// console.log(age * grace)
+// console.log(age / grace)
+// console.log(age ** grace)
+/*
+
+i am a 
+mutipler line
+ comment
+
+
+
+*/
+
+if(age == 18){
+    console.log("you can drive")
+
+}
+else if( age == 0){
+    console.log("are you kidding")
+}
+else if( age == 1){
+    console.log("are you again kidding")
+}
+else{
+    console.log("yon can not drive")
+}
